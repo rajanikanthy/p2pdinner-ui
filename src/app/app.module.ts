237b1c5e7ce8f3f3151ea6e7213971f82bdeabbe
Rajanikantha <rkant04@safeway.com>
2018-11-24 07:56:46 -0800

    adding registration events

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -24,7 +25,8 @@ import { MenuItemComponent } from './menu/menu-item/menu-item.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  emailAddress: string = "example@safeway.com";
+  password: string;
+  showRegistrationForm = false;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onLogin() {
+    console.log("Login handler invoked");
+    console.log("Email Address: " + this.emailAddress);
+  }
+
+  onRegister() {
+    this.showRegistrationForm = true;
+    console.log("Register handler invoked");
+  }
+
+  onReset() {
+    console.log("Reset handler invoked");
+  }
+
+  onRegisterSuccess() {
+    this.showRegistrationForm = !this.showRegistrationForm;
+  }
+
+  onRegisterCancel() {
+    this.showRegistrationForm = !this.showRegistrationForm;
   }
 
 }
